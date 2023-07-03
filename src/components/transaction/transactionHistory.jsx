563e4841acc0f7f3a0b93f90ba335f2c81d85a16
Rafał Biżek <49203973+RafalBizek@users.noreply.github.com>
2023-07-03 20:45:58 +0200

    fixbug transactionHistry component

--- src/components/transaction/transactionHistory.jsx
+++ src/components/transaction/transactionHistory.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import './transactionHistoryCss.css';
+import css from './transactionHistory.module.css';
 
 const TransactionHistory = ({ items }) => {
   return (
-    <table className="transaction-history">
+    <table className={css.transactionHistory}>
       <thead>
         <tr>
           <th>Type</th>
